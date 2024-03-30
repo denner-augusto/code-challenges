627ced4fa2833bf095437f4c8aadbfd32416b9c0
Denner Augusto <63256030+denner-augusto@users.noreply.github.com>
2024-03-30 17:16:24 +0000

    Coding challenge 1480 - LeetCode

---
+++ leetcode/1480. Running Sum of 11dArray/script.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    # list = [1,2,3,4]
+    def runningSum(self, nums):
+        # 1.Store the list e.g.: [1,2,3]
+        # 2. Read the first position [i] and store the number in other list
+        # 3. Read the second number [i+1] and sum with the last position [i-1]
+        # 4. Repeat the process until the last position len(list) -1
+        # 5. 
+
+        return list
+Solution.runningSum([1,2,3,4])
+"""
+:type nums: List[int]
+:rtype: List[int]
+"""
