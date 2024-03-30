a75789c7d5e2145aa10bdc3410ad34e74a2d61f3
Denner Augusto <63256030+denner-augusto@users.noreply.github.com>
2024-03-30 19:27:25 +0000

    Complete code challenge 1480.

--- leetcode/1480. Running Sum of 11dArray/script.py
+++ leetcode/1480. Running Sum of 11dArray/script.py
@@ -1,14 +1,29 @@
 class Solution(object):
-    # list = [1,2,3,4]
     def runningSum(self, nums):
         # 1.Store the list e.g.: [1,2,3]
-        # 2. Read the first position [i] and store the number in other list
-        # 3. Read the second number [i+1] and sum with the last position [i-1]
-        # 4. Repeat the process until the last position len(list) -1
-        # 5. 
+        output_arr = []
+        # 2. Repeat the process until the last position len(list) 
+        for i in range(0, len(nums) -1 ):
+        # 3. If is the first position read the first element [i] and store the number in other list
+            if i == 0:
+                output_arr.append(nums[i])
+                output_arr.append(nums[i] + nums[i+1])
+        # 4. If is other position than the first one, sum the current element [i] and sum with the last element [i-1]
+            else:
+                output_arr.append(output_arr[i] + nums[i+1])        
+        # 5. -1 and return the output array
+        return output_arr
+# Define the Running sun array
+input_arr = [3,1,2,10,1]
+# Object to store the solution
+solution = Solution()
+# Call the function passing the list
+result = solution.runningSum(input_arr)
+# Print the return
+print(result)
 
-        return list
-Solution.runningSum([1,2,3,4])
+
+
 """
 :type nums: List[int]
 :rtype: List[int]
