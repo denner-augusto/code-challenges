7f7ecc864c2071d2dddc435c16125c243e5ac396
denner-augusto <denner.augusto300@gmail.com>
2024-04-03 13:19:42 -0300

    C Hello World solution

---
+++ c_lang_notes.md
@@ -0,0 +1,13 @@
+# C Language notes
+
+To create a new program, first we need to create the script file to be compiled
+
+```nano path/file_name.c && cd $_```
+
+Then we can create the script into the .c file and compile it.
+
+```gcc file_name.c - file_name```
+
+And finally execute it
+
+``` file_name.exe```
---
+++ exercism/hello-world/test_hello_world.c
@@ -0,0 +1,25 @@
+#include <stddef.h>
+#include "unity.h"
+#include "hello_world.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+static void test_hello(void)
+{
+    TEST_ASSERT_EQUAL_STRING("Hello, World!"), hello());
+}
+
+int main(void)
+{
+    UnityBegin("test/test_hello_world.c");
+
+    RUN_TEST(test_hello);
+
+    return UnityEnd();
+}
