3e8211c1f923855cc141e5324ac189005092dc5a
denner-augusto <denner.augusto300@gmail.com>
2024-04-18 11:27:47 -0300

    Solution Bob 2nd Iteration exercism

--- exercism/python/bob/bob.py
+++ exercism/python/bob/bob.py
@@ -1,9 +1,11 @@
 def response(hey_bob: str) -> str: 
-    def is_question(hey_bob: str) -> bool:
-        return True if hey_bob != "" and hey_bob[-1] == "?" else False
-    return ("Calm down, I know what I'm doing!" if is_question(hey_bob) and hey_bob.isupper()\
-            else "Fine. Be that way!" if hey_bob.isspace() or hey_bob == ""\
-            else "Sure." if is_question(hey_bob.strip())\
-            else "Whoa, chill out!" if hey_bob.isupper()\
-            else "Whatever."\
-            )
+    hey_bob_strip = hey_bob.rstrip()
+    is_question = hey_bob_strip.endswith("?")
+    is_yelling = hey_bob_strip.isupper()
+    if not hey_bob_strip or hey_bob_strip.isspace():
+        return "Fine. Be that way!"
+    elif is_question and is_yelling:
+        return "Calm down, I know what I'm doing!"
+    elif is_question:
+        return "Sure."
+    return "Whoa, chill out!" if is_yelling else "Whatever." 
