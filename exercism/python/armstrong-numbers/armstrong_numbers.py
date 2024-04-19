493ac6f26163f2e8289d888dbc0ccdcfe75925fb
denner-augusto <denner.augusto300@gmail.com>
2024-04-19 08:35:23 -0300

    Solution Armstrong Numbers 6th iteration exercism

--- exercism/python/armstrong-numbers/armstrong_numbers.py
+++ exercism/python/armstrong-numbers/armstrong_numbers.py
@@ -2,13 +2,13 @@ def is_armstrong_number(number: int) -> bool:
     """
     Identify if the provided number is a armstrong number
     
-    :number: int - number to be analyzed if its a armstrong number or not
-    :return: bool - true if a number is a armstrong number and false if isn`t
+    :number: int - Input number provided by the user
+    :return: bool - Boolean return with True for a armstrong number and False if is not.
     """
     str_number = str(number)
     
     # 3. Iterate in the digits of the number, in which for each digit, we will power the current digit by number lenght
-    power_digitis_sum = sum(int(digit)**len(str_number) for digit in str_number)
+    power_digits_sum = sum(int(digit)**len(str_number) for digit in str_number)
 
-    return power_digitis_sum == number
+    return power_digits_sum == number
 
