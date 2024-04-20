a40b275760bfd0fcf19e517471ed3f8422752503
denner-augusto <denner.augusto300@gmail.com>
2024-04-20 13:57:10 -0300

    Solution Darts 2nd Iteration exercism

--- exercism/python/darts/darts.py
+++ exercism/python/darts/darts.py
@@ -1,5 +1,7 @@
 import math
-def score(x, y):
-    scores = ((10.01,0),(5.01,1),(1.01,5),(0,10)) #(distance, score)
-    dart_distance = math.sqrt(x**2 + y**2)
-    return next(final_score for distance, final_score in scores if dart_distance >= distance)
+
+
+def score(x: float, y: float) -> int:
+    scores = [[10, 0], [5, 1], [1, 5], [0, 10]] #[distance, score]
+    dart_distance = math.sqrt(x ** 2 + y ** 2)
+    return next((final_score for distance, final_score in scores if dart_distance > distance), 10)
